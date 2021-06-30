72f4651b57e10b3bfb110063d352f95140a77100
Dmitriy Gryaznykh <gry-dmitrij@yandex.ru>
2021-06-30 16:31:57 +0300

    - Сделана экранная клавиатура

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <div class="display">
-      <input type="number" v-model.number="operand1">
-      <input type="number" v-model.number="operand2">
+      <input type="number" v-model.number="operand1" @focus="operandNum = 'operand1'">
+      <input type="number" v-model.number="operand2" @focus="operandNum = 'operand2'">
       = {{result}}
     </div>
     <div class="buttons">
@@ -11,7 +11,20 @@
     <div class="error" v-show="error">
       Ошибка: {{ error }}
     </div>
-
+    <div class="keyboard">
+      <input type="checkbox" id="keyboard" v-model="keyboardShow">
+      <label for="keyboard">Отобразить экранную клавиатуру</label>
+      <div class="keys" v-show="keyboardShow">
+        <button v-for="key in keys" :key="key" @click="activeKey(key)">{{ key }}</button>
+        <br>
+        <input type="radio" id="op1" name="operands" value="operand1" v-model="operandNum"
+               :checked="operandNum === 'operand1'">
+        <label for="op1">Операнд 1</label>
+        <input type="radio" id="op2" name="operands" value="operand2" v-model="operandNum"
+               :checked="operandNum === 'operand1'">
+        <label for="op2">Операнд 2</label>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -20,11 +33,14 @@ export default {
   name: 'Calculator',
   data() {
     return {
+      operandNum: 'operand1',
       operand1: 0,
       operand2: 0,
       result: 0,
       error: '',
-      buttons: ['+', '-', '*', '/', '^', 'int /']
+      buttons: ['+', '-', '*', '/', '^', 'int /'],
+      keys: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '←', 'C'],
+      keyboardShow: false
     }
   },
   methods: {
@@ -71,7 +87,45 @@ export default {
       this.result = Math.pow(this.operand1, this.operand2);
     },
     intDiv() {
+      if (this.operand2 === 0) {
+        this.error = 'На 0 делить нельзя!'
+        return;
+      }
       this.result = Math.round(this.operand1 / this.operand2);
+    },
+    activeKey(key) {
+      const num = +key;
+      if (isFinite(num)) {
+        this.numberActive(num);
+        return;
+      }
+      switch (key) {
+        case '←':
+          this.backspace();
+          break;
+        case 'C':
+          this.clear();
+          break;
+      }
+    },
+    numberActive(num) {
+      const operand = this.operandNum;
+      if (this[operand] !== 0) {
+        this[operand] = +(this[operand].toString() + num);
+      } else {
+        this[operand] = num;
+      }
+    },
+    backspace() {
+      const operand = this.operandNum;
+      if (this[operand] !== 0) {
+        this[operand] = +this[operand].toString().slice(0, -1);
+      }
+    },
+    clear() {
+      this.operand1 = 0;
+      this.operand2 = 0;
+      this.result = 0
     }
   }
 }
